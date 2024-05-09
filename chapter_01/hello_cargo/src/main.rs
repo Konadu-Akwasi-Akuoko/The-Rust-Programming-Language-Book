ff1be35bf0ca8015808af971d5863897ff0c49e8
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-09 16:37:23 +0000

    feat: created a new project

---
+++ chapter_01/hello_cargo/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    print_name("Akwasi Konadu");
+}
+
+fn print_name(name: &str) {
+    println!("My name is {}", name)
+}
---
+++ chapter_01/hello_cargo/target/debug/deps/hello_cargo-52e28eb33ab7fcf9.d
@@ -0,0 +1,5 @@
+/home/akwasi_akuoko/Projects/Rust/the-rust-programming-language/chapter_01/hello_cargo/target/debug/deps/libhello_cargo-52e28eb33ab7fcf9.rmeta: src/main.rs
+
+/home/akwasi_akuoko/Projects/Rust/the-rust-programming-language/chapter_01/hello_cargo/target/debug/deps/hello_cargo-52e28eb33ab7fcf9.d: src/main.rs
+
+src/main.rs:
